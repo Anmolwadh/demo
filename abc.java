b3669627a7f3ea657b9aa5b30f05fc3091256e23
Bhinder <bhinder@signitysolutions.com>
2023-05-03 17:35:43 +0530

    Commit message

--- abc.java
+++ abc.java
@@ -2,17 +2,20 @@ package Demoa;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class abc {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.setProperty("webdriver.chrome.driver", "C://Users//hp//Downloads//chromedriver_win32//chromedriver.exe");	
-		ChromeDriver driver= new ChromeDriver();
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\hp\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		WebDriver driver= new ChromeDriver(options);
 		driver.get("https://nuco-development.signitydemo.in/login");
 
-		System.out.println("Hello");
+		
 	}
 
 }
